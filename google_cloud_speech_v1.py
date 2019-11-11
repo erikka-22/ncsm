2a7b903ff2d31ed90a88f6c9b31093b23cdd435e
erikka-22 <iwasaki.pingoo@gmail.com>
2019-11-11 23:42:19 +0900

    入力したテキストをjson形式で記録することができた．ファイル名が書き換わっちゃうので，そこの修正が必要

--- google_cloud_speech_v1.py
+++ google_cloud_speech_v1.py
@@ -70,20 +70,25 @@ def schedule(interval: float, wait: bool):
 class MicrophoneStream(object):
     """Opens a recording stream as a generator yielding the audio chunks."""
 
-    def __init__(self, rate: int, chunk: int):
+    def __init__(self, rate, chunk):
         self._rate = rate
         self._chunk = chunk
 
+        # Create a thread-safe buffer of audio data
         self._buff = queue.Queue()
         self.closed = True
 
     def __enter__(self):
-        print("test")
         self._audio_interface = pyaudio.PyAudio()
         self._audio_stream = self._audio_interface.open(
             format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            # https://goo.gl/z757pE
             channels=1, rate=self._rate,
             input=True, frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
             stream_callback=self._fill_buffer,
         )
 
@@ -92,10 +97,11 @@ class MicrophoneStream(object):
         return self
 
     def __exit__(self, type, value, traceback):
-        print("exit")
         self._audio_stream.stop_stream()
         self._audio_stream.close()
         self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
         self._buff.put(None)
         self._audio_interface.terminate()
 
@@ -107,25 +113,26 @@ class MicrophoneStream(object):
     def generator(self):
         global message_from_processing
         global can_speechrec_flag
-        global stock
         while not self.closed:
             if message_from_processing == "end":
                 can_speechrec_flag = False
                 print("end")
                 break
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
             chunk = self._buff.get()
             if chunk is None:
                 return
-            stock.append(chunk)
             data = [chunk]
 
+            # Now consume whatever other data's still buffered.
             while True:
                 try:
                     chunk = self._buff.get(block=False)
 
                     if chunk is None:
                         return
-                    stock.append(chunk)
                     data.append(chunk)
                 except queue.Empty:
                     break
@@ -133,7 +140,7 @@ class MicrophoneStream(object):
             yield b''.join(data)
 
 
-def listen_print_loop(responses):
+def listen_print_loop(responses, buffer):
     """Iterates through server responses and prints them.
     The responses passed is a generator that will block until a response
     is provided by the server.
@@ -147,16 +154,24 @@ def listen_print_loop(responses):
     """
     num_chars_printed = 0
     for response in responses:
-
         if not response.results:
             continue
 
+        # The `results` list is consecutive. For streaming, we only care about
+        # the first result being considered, since once it's `is_final`, it
+        # moves on to considering the next utterance.
         result = response.results[0]
         if not result.alternatives:
             continue
 
+        # Display the transcription of the top alternative.
         transcript = result.alternatives[0].transcript
 
+        # Display interim results, but with a carriage return at the end of the
+        # line, so subsequent lines will overwrite them.
+        #
+        # If the previous result was longer than this one, we need to print
+        # some extra spaces to overwrite the previous result
         overwrite_chars = ' ' * (num_chars_printed - len(transcript))
 
         if not result.is_final:
@@ -168,6 +183,8 @@ def listen_print_loop(responses):
         else:
             print(transcript + overwrite_chars)
 
+            # Exit recognition if any of the transcribed phrases could be
+            # one of our keywords.
             if re.search(r'\b(exit|quit)\b', transcript, re.I):
                 print('Exiting..')
                 break
@@ -177,7 +194,7 @@ def listen_print_loop(responses):
             # 最終認識結果をリストに追加
             to_pcg.append(recognizedText)
 
-            text.divideText(recognizedText)
+            text.divideText(recognizedText, buffer)
 
             schedule(0.25, False)
 
@@ -185,8 +202,13 @@ def listen_print_loop(responses):
 
 
 def speechRecognition():
-    print("Start SpeechRecognition")
+    global charBuff
+
+    print("hello")
+    # See http://g.co/cloud/speech/docs/languages
+    # for a list of supported languages.
     language_code = 'ja-JP'  # a BCP-47 language tag
+
     client = speech.SpeechClient()
     config = types.RecognitionConfig(
         encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
@@ -204,7 +226,10 @@ def speechRecognition():
                     for content in audio_generator)
 
         responses = client.streaming_recognize(streaming_config, requests)
-        listen_print_loop(responses)
+        print(responses)
+
+        # Now, put the transcription responses to use.
+        listen_print_loop(responses, charBuff)
 
 
 def main():
