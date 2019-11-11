148ba4ac15e1c29d9d383fbccd4e9a3b8197a6d6
erikka-22 <iwasaki.pingoo@gmail.com>
2019-11-11 18:01:03 +0900

    コミットする

--- google_cloud_speech_v1.py
+++ google_cloud_speech_v1.py
@@ -134,6 +134,17 @@ class MicrophoneStream(object):
 
 
 def listen_print_loop(responses):
+    """Iterates through server responses and prints them.
+    The responses passed is a generator that will block until a response
+    is provided by the server.
+    Each response may contain multiple results, and each result may contain
+    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+    print only the transcription for the top alternative of the top result.
+    In this case, responses are provided for interim results as well. If the
+    response is an interim one, print a line feed at the end of it, to allow
+    the next result to overwrite it, until the response is a final one. For the
+    final one, print a newline to preserve the finalized transcription.
+    """
     num_chars_printed = 0
     for response in responses:
 
